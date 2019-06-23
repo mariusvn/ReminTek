8e666fd7e7c4b8046c451a052adc6dbc79e7c220
Marius Van Nieuwenhuyse <mariusvnspam@gmail.com>
2019-06-23 13:59:20 +0200

    ouii

---
+++ pages/Connect.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class ConnectPage extends Component {
+    render() {
+        return (
+            <View>
+                <Text>ReminTek</Text>
+            </View>
+        )
+    }
+}
