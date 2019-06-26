9f00b322b9359f492c49dd669126fd234a4ccc7a
Marius Van Nieuwenhuyse <marius.van-nieuwenhuyse@epitech.eu>
2019-06-26 10:20:47 +0200

    server error handling

--- server/controllers/module.js
+++ server/controllers/module.js
@@ -5,6 +5,13 @@ module.exports = {
 	get: async (req, res) => {
 		if (!req.params.year || !req.params.module || !req.params.instance)
 			res.sendStatus(400).json({message: "missing parameter(s)"});
-		res.json(JSON.parse(await epitech.getModule(req.params.year, req.params.module, req.params.instance)));
+		try {
+			res.json(JSON.parse(await epitech.getModule(req.params.year, req.params.module, req.params.instance)));
+		} catch (e) {
+			res.json({
+				title: req.params.module + " doesn't exists",
+				end: new Date().toISOString()
+			})
+		}
 	}
 };
